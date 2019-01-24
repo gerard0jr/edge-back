267b7e7dd3b5366b56d912f3feb938bdba788e23
Gerardo <gera@MacBook-de-Gerardo.local>
2019-01-23 22:22:59 -0600

    Add react files

---
+++ public/precache-manifest.d6fc8bfa43fe8ec0cf8ca4789d019128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95e69c96445fb408724c",
+    "url": "/static/js/main.95e69c96.chunk.js"
+  },
+  {
+    "revision": "a07e52647c677a38c0ff",
+    "url": "/static/js/1.a07e5264.chunk.js"
+  },
+  {
+    "revision": "95e69c96445fb408724c",
+    "url": "/static/css/main.f12104de.chunk.css"
+  },
+  {
+    "revision": "04d297d5340f1bc02f3c3df0c55308a3",
+    "url": "/index.html"
+  }
+];
